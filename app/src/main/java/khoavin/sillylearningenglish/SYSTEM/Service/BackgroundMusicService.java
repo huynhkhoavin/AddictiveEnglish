d062f3843f5d24799a386c62fe523c02a6a94a79
Huynh Khoa Vin <khoavin@gmail.com>
2017-04-16 18:54:32 +0700

    -new

--- app/src/main/java/khoavin/sillylearningenglish/SYSTEM/Service/BackgroundMusicService.java
+++ app/src/main/java/khoavin/sillylearningenglish/SYSTEM/Service/BackgroundMusicService.java
@@ -53,10 +53,10 @@ public class BackgroundMusicService extends Service {
         if (intent.getAction().equals(Constants.ACTION.STARTFOREGROUND_ACTION)) {
             try {
                 mMediaPlayer = new MediaPlayer();
-                mMediaPlayer.setDataSource("http://192.168.1.101/Resources/OBL%St1%Love%or%Money/Rowena%Akinyemi%-%Love%or%Money%7.mp3'");
+                mMediaPlayer.setDataSource("http://192.168.1.101/resources/OBL%20St1%20A%20Ghost%20in%20Love%20and%20Other%20Plays/01.mp3");
                 mMediaPlayer.prepare();
                 playState = PLAYSTATE.IS_PAUSE;
-                EventBus.getDefault().post(mMediaPlayer);
+
             } catch (IOException e) {
                 e.printStackTrace();
                 Toast.makeText(getApplicationContext(), "Can't connect to server", Toast.LENGTH_SHORT).show();
@@ -82,6 +82,8 @@ public class BackgroundMusicService extends Service {
     public void playAction(){
         mMediaPlayer.start();
         playState = PLAYSTATE.IS_PLAYING;
+        EventBus.getDefault().post(PLAYSTATE.IS_PLAYING);
+        EventBus.getDefault().post(mMediaPlayer);
     }
     public void pauseAction(){
         if (mMediaPlayer.isPlaying()) {
